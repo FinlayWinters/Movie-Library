1e4375ea7908805b3ac5b179cc8eb551c6814d78
FinlayWinters <50027110@serc.ac.uk>
2024-05-01 14:30:23 +0100

    intial

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -25,43 +25,100 @@ public class UserInterface
 
             switch (userInput)
             {
-                case "1";
+                case "1":
                 addMovie();
                     break;
-                case "2";
+                case "2":
                 //this.showAllMovies();
                     break;
-                case "3";
+                case "3":
                 //this.movieCount();
                     break;
                 case "0",
-                quit = true;
+                quit = true:
                 break;
-            default;
+            default
             System.out.println("That was not a valid choice");
 
 
             }
         }
     }
-    public void addMovie()
-    {
+    public void addMovie() {
         System.out.println("Please enter the title of the movie: ");
         String title = reader.getInput();
 
         System.out.println("Please enter the running time of the movie: ");
         String userInput = reader.getInput();
-        int runningTime = 0:
+        int runningTime = 0;
 
-        try
-        {
+        try {
             runningTime = Integer.parseInt(userInput);
-        }
-        catch(NumberFormatException exception)
-        {
+        } catch (NumberFormatException exception) {
             System.err.println("The input could not be parse to an int");
             return;
         }
+
+        Genre aGenre;
+
+        System.out.println("Please choose from the following;");
+        System.out.println("1. Comedy:");
+        System.out.println("2. Thriller:");
+        System.out.println("3. Horror:");
+
+        userInput = reader.getInput();
+
+        int genreNumber;
+        -1;
+
+        try {
+            runningTime = Integer.parseInt(userInput);
+        } catch (NumberFormatException exception) {
+            System.err.println("The input could not be parse to an int");
+            return;
+        }
+
+        if (genreNumber < 1 || genreNumber > 6) {
+            System.err.println("That was not a valid option");
+            return;
+        }
+
+        Genre genre;
+
+        switch (genreNumber) {
+            case 1:
+                genre = Genre.COMEDY;
+                break;
+            case 2:
+                genre = Genre.HORROR;
+                break;
+            case 3:
+                genre = Genre.THRILLER;
+                break;
+        }
+
+        Movie m = new Movie(title, runningTime, genre);
+        this.Library.addMovie(m);
     }
-}
+
+    public void listMovies()
+    {
+        this.Library.showMovies();
+    }
+
+    public void showMovieCount()
+    {
+        int count = this.Library.movieCount();
+
+        System.out.println(count);
+    }
+
+
+    }
+
+
+
+
+
+
 
